ff8abb5ebb6099b22bda4f7c8b89e9536fd3a064
zenqst <zenqst@gmail.com>
2024-05-22 22:23:16 +0300

    fix: link in api/config

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:3001/api'
+export const BASE_URL = 'https://pindie-backend-zenqst.nomoredomainswork.ru/'
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
