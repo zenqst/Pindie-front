df5fb047ed9ddf55404497683b2b9501946df6fd
zenqst <zenqst@gmail.com>
2024-05-25 17:16:56 +0300

    fix: config.js (api)

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://pindie-backend-zenqst.nomoredomainswork.ru/'
+export const BASE_URL = 'https://pindie-backend-zenqst.nomoredomainswork.ru/api'
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
